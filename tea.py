814bd1e2865a23777db52a31c0f9ed83442431d5
Aniket patel <ceaniket@gmail.com>
2017-03-18 00:11:49 +0530

    change old url to new url in tea.py

--- tea.py
+++ tea.py
@@ -6,12 +6,12 @@ from json import loads
 _url="35.164.228.234"
 def mytea(name):
     global _url
-    msg = get('http://{url}:5000/countme/{name}'.format(url=_url,name=name))
+    msg = get('http://{url}:5000/mytea/{name}'.format(url=_url,name=name))
     return msg.content
     
 def tealist():
     global _url
-    teal = get('http://{url}:5000/listtea'.format(url=_url))
+    teal = get('http://{url}:5000/tealist'.format(url=_url))
     tea=loads(teal.content)
     print tea
     print "total cups of tea =>",len(tea)
