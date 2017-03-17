e41533188e3ef6d098e63925fab282f4b2b11f2c
Aniket patel <ceaniket@gmail.com>
2017-03-17 12:51:40 +0530

    add timer event to clear teateas list. react on UTC time.

--- teacounter.py
+++ teacounter.py
@@ -1,8 +1,18 @@
 from flask import Flask
 import json
+import schedule
+import time
+
 app = Flask(__name__)
 
 teateas=[]
+
+def clearteateas():
+    global teateas
+    teateas=[]
+
+schedule.every().day.at("03:30").do(clearteateas)
+schedule.every().day.at("09:00").do(clearteateas)
 
 @app.route('/countme/<name>')
 def countme(name):
